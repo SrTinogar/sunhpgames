9cd30305c7adac59afb7433ad34de025587d815e
Brizzly <jadeware@gmail.com>
2017-03-23 00:46:07 +0100

    smhp ed

---
+++ HP50/SuperMarioHP50/editor/smhp50ed.hpp
@@ -0,0 +1,32 @@
+[Main]
+library=1
+IncludeUserRPL=1
+AutoLoad=0
+Compress=0
+Calc48=0
+KeepAll=0
+UseDefaultEntryList=1
+WarnRamChanges=1
+LoadOnly=0
+EMUVisible=0
+ProjectTab=1
+InformHeight=50
+EntryPointList=
+Title=New_Project
+Config=
+ExtPrg=
+Message=
+Romid=200
+Slot=0
+Emu48Ram=C:\Users\julien\Documents\Debug4x\Emu\default 48GX.e48
+Emu48Port2=C:\Program Files (x86)\Hewlett-Packard\Debug4x\Emu\default 48GX.bin
+Emu49Ram=C:\Users\julien\Documents\Debug4x\Emu\default 50G.e49
+FilePath=smhp50ed.hp
+DirPath=
+RPL_INCLUDE=
+SasmParams=
+LRFiles=
+[Files]
+file1=smhp_editor_50.s
+[Edit]
+edit2=>smhp_editor_50.s
